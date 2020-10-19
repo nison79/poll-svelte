54c04ff4ccbd4bc7d50a06c474040967ff366d36
nison79 <georgenison@gmail.com>
2020-10-19 20:28:26 +0300

    first commit

---
+++ poll-svelte/src/components/Header.svelte
@@ -0,0 +1,25 @@
+
+
+<header>
+    <h1>
+        <img src='/img/logo.jpg' alt='poll ninja logo'>
+    </h1>
+
+</header>
+
+
+<style>
+    header {
+        background-color: #f7f7f7;
+        padding:20px;
+    }
+    header img {
+        max-width:200px;
+    }
+
+    h1{
+        margin:0;
+        text-align:center;
+    }
+
+</style>
